163130795fd4532629342db6da6c2344eb16b4d1
tkhuynh <thequest2311@yahoo.com>
2015-12-15 14:58:18 -0800

    complete challenges

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,13 +10,42 @@ class PetsController < ApplicationController
 
   def create
     pet_params = params.require(:pet).permit(:name, :breed)
-    pet = Pet.create(pet_params)
-    redirect_to pet_path(pet)
+    pet = Pet.new(pet_params)
+    if pet.save
+      redirect_to pet_path(pet)
+    else
+      flash[:error] = pet.errors.full_messages.join(", ") 
+      redirect_to new_pet_path
+    end
   end
 
   def show
     pet_id = params[:id]
     @pet = Pet.find_by_id(pet_id)
+  end
+
+  def edit
+    pet_id = params[:id]
+    @pet = Pet.find_by_id(pet_id)
+    render :edit
+  end
+
+  def update
+    pet_id = params[:id]
+    pet = Pet.find_by_id(pet_id)
+    pet_params = params.require(:pet).permit(:name, :breed)
+    pet.update_attributes(pet_params)
+    if pet.errors.any?
+      flash[:error] = pet.errors.full_messages.join(", ") 
+      redirect_to new_pet_path
+    else
+      redirect_to edit_pet_path(pet)
+    end
+  end
+
+  def destroy
+    pet_id = params[:id]
+    Pet.find_by_id(pet_id).destroy
   end
 
 end
